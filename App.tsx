feaf26c81e9e817fb839e718a1c063f43118e727
fnapp <106439658+fatmayddin@users.noreply.github.com>
2023-02-27 10:04:16 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import {View, Text } from 'react-native'
+
+
+export default class App extends Component{
+
+  constructor(props:any){
+    super(props)
+
+  }
+
+
+  render(): React.ReactNode {
+    return <View>
+      <Text>deneme</Text>
+    </View>
+  }
+}
